60e47c75ed8ce166928adbb4b65feb166e95e60c
labviewtab1234 <labviewtab@gmail.com>
2025-06-04 02:54:22 +0200

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: "/Genuis/",
   server: {
     host: "::",
     port: 8080,
